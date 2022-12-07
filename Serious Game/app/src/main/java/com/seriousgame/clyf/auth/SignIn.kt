7e8f672b5f261e18e01017d351732b775236e217
FiliBell <117976345+FiliBell@users.noreply.github.com>
2022-12-07 13:26:32 +0100

    Firebase database insert

--- Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignIn.kt
+++ Serious Game/app/src/main/java/com/seriousgame/clyf/auth/SignIn.kt
@@ -1,19 +1,22 @@
 package com.seriousgame.clyf.auth
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.seriousgame.clyf.MainActivity
-import com.seriousgame.clyf.R
 import com.seriousgame.clyf.databinding.ActivitySignInBinding
-import com.seriousgame.clyf.databinding.ActivitySignUpBinding
+import java.util.Objects
+
 
 class SignIn : AppCompatActivity() {
 
     private lateinit var binding: ActivitySignInBinding //feature that allows you to more easily write code that interacts with views (replaces findViewById) - F.
     private lateinit var firebaseAuth: FirebaseAuth //define FirebaseAuth - F.
+    private lateinit var db : FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,7 @@ class SignIn : AppCompatActivity() {
 
         firebaseAuth = FirebaseAuth.getInstance()
         //assign to firebaseAuth a method that returns an instance of this class corresponding to the default FirebaseApp instance - F.
+        db = FirebaseFirestore.getInstance()    // Access a Cloud Firestore instance from your Activity
 
         binding.signinButton.setOnClickListener {   //at signInButton click - F.
             val email = binding.emailET.text.toString() //email field - F.
@@ -34,6 +38,22 @@ class SignIn : AppCompatActivity() {
 
                         //1st inner if - S.
                         if (it.isSuccessful) { //succesful case - S.
+<<<<<<< Updated upstream
+=======
+
+                            //insert the user information into the MutableMap
+                            var informazioni : MutableMap<String, Any> = hashMapOf()
+                            informazioni["Email"] = email
+                            informazioni["Pass"] = pass
+
+                            //insert the MutableMap (information) into the MutableMap (dati)
+                            var dati : MutableMap<String, Any>  = hashMapOf()
+                            dati["Dati"] = informazioni
+
+                            //add a new document with a generated ID and insert into it dati
+                            db.collection("Users").document().set(dati)
+
+>>>>>>> Stashed changes
                             val intent = Intent(this, MainActivity::class.java) //define intent - S.
                             startActivity(intent) //takes you to SignIn activity - S.
                         } else {
@@ -51,5 +71,7 @@ class SignIn : AppCompatActivity() {
             val intent = Intent(this, SignUp::class.java)
             startActivity(intent)
         }
+
+
     }
 }
